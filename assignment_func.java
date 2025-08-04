ee13595ee79b981143cf04dfdcbd8e5084e53ff9
dhruvshukla04 <124866014+dhruvshukla04@users.noreply.github.com>
2025-08-04 22:07:32 +0530

    Add files via upload

---
+++ assignment_func.java
@@ -0,0 +1,108 @@
+import java.util.*;
+public class assignment_func {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        1. Number Guessing Game
+
+//        2. Maximum of Three Numbers
+//            int a = sc.nextInt();
+//            int b = sc.nextInt();
+//            int c = sc.nextInt();
+//
+//            if (a>b && a>c) {
+//                System.out.println(a);
+//            }
+//            else if (b>a && b>c) {
+//                System.out.println(b);
+//            }
+//            else if (c>a && a>b) {
+//                System.out.println(c);
+//            }
+//        }
+//    }
+
+//        3. Prime Number Checker
+//            int num = sc.nextInt();
+//            boolean prime = true;
+//            if (num <= 1) prime = false;
+//
+//            for (int i = 2; i <= num / 2; i++) {
+//                if (num % i == 0) {
+//                    prime = false;
+//                    break;
+//                }
+//            }
+//
+//            if (prime)
+//                System.out.println(num + " is a prime number.");
+//            else
+//                System.out.println(num + " is not a prime number.");
+//        }
+//    }
+
+//        4. Fibonacci Sequence Generator
+//            int n = sc.nextInt();
+//            int a = 0, b = 1;
+//            for (int i = 1; i <= n; i++) {
+//                System.out.print(a + " ");
+//                int next = a + b;
+//                a = b;
+//                b = next;
+//            }
+//        }
+//    }
+
+//        5. Palindrome Checker
+//            String word = sc.nextLine();
+//            String rev = "";
+//            for (int i = word.length() - 1; i >= 0; i--) {
+//                rev = rev + word.charAt(i);
+//            }
+//
+//            if (word.equalsIgnoreCase(rev))
+//                System.out.println("It is a palindrome.");
+//            else
+//                System.out.println("It is not a palindrome.");
+//        }
+//    }
+
+//        6. Factorial Using Recursion
+
+
+//        7. GCD and LCM Calculator
+//            int a = sc.nextInt();
+//            int b = sc.nextInt();
+//
+//            int gcd = 1;
+//            for (int i = 1; i <= a && i <= b; i++) {
+//                if (a % i == 0 && b % i == 0)
+//                    gcd = i;
+//            }
+//
+//            int lcm = (a * b) / gcd;
+//
+//            System.out.println(gcd);
+//            System.out.println(lcm);
+//        }
+//    }
+
+//        8. Temperature Converter
+
+//        9. Basic Calculator
+//        double a = sc.nextDouble();
+//        double b = sc.nextDouble();
+//
+//        char op = sc.next().charAt(0);
+//        double result = 0;
+//
+//        if (op == '+') {
+//            System.out.println(a + b);
+//        } else if (op == '-') {
+//            System.out.println(a - b);
+//        } else if (op == '*') {
+//            System.out.println(a * b);
+//        } else if (op == '/') {
+//            System.out.println(a / b);
+//        }
+    }
+}
